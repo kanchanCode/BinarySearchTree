7e57327ec85012db472a1a3406ad8103aacff827
kanchanCode <jkanchan2911@gmail.com>
2021-12-03 20:03:07 +0530

    ADDED size

--- binaryTree.cs
+++ binaryTree.cs
@@ -9,6 +9,7 @@ namespace Binary_Search_Tree
       public T NodeData { get; set; }
       public binaryTree<T> LeftTree { get; set; }
       public binaryTree<T> RightTree { get; set; }
+        int n = 0;
       public binaryTree(T nodeData)
         {
             this.NodeData = nodeData;
@@ -35,6 +36,7 @@ namespace Binary_Search_Tree
                 else
                     this.RightTree.insert(item);
             }
+            n = n + 1;
         }
 
         public void Display()
@@ -42,20 +44,29 @@ namespace Binary_Search_Tree
             if (this.LeftTree != null)
             {
                 this.leftCount++;
-                Console.Write("L"+leftCount);
                 this.LeftTree.Display();
             }
             Console.Write(this.NodeData.ToString()+" ");
             if(this.RightTree != null)
             {
                 this.rightCount++;
-                Console.Write("R" + rightCount);
-
                 this.RightTree.Display();
             }
-
         }
 
+        public void GetSize()
+        {
+            this.leftCount += 1;
+            this.rightCount += 1;
+            int k = n + 1;
+            Console.WriteLine("\nsize or number of nodes "+k);
+            Console.WriteLine("\nHeight of BST is"+" "+(1+this.leftCount+this.rightCount));
+        }
 
+        /*public void ifExist(T element,binaryTree<T> node)
+        {
+            if(node==null)
+                return false;
+        }*/
     }
 }
