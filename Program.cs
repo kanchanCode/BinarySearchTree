ac3510f634c656502201dc66be2708dbb756f067
kanchanCode <jkanchan2911@gmail.com>
2021-12-03 19:10:59 +0530

    ADDED main

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree!");
+            binaryTree<int> binaryObj = new binaryTree<int>(56);
+            binaryObj.insert(30);
+            binaryObj.insert(70);
+            binaryObj.insert(22);
+            binaryObj.insert(40);
+            binaryObj.insert(60);
+            binaryObj.insert(95);
+            binaryObj.insert(65);
+            binaryObj.insert(63);
+            binaryObj.insert(67);
+            binaryObj.insert(11);
+            binaryObj.insert(3);
+            binaryObj.insert(16);
+
+            binaryObj.Display();
+
+        }
+    }
+}
